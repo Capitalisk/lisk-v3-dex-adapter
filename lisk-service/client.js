f3cced65b412306eb8915c11e034d786558985c8
sacOO7 <sachinshinde7676@gmail.com>
2021-09-12 01:20:24 +0530

    Refactored HTTP client

--- lisk-service/client.js
+++ lisk-service/client.js
@@ -2,57 +2,58 @@ const axios = require('axios');
 
 class HttpClient {
 
-    static HttpGetRequestFn = (path, params) => (baseUrl) => axios.get(`${baseUrl}${path}`, {params});
-    static HttpPostRequestFn = (path, payload) => (baseUrl) => axios.post(`${baseUrl}${path}`, payload);
-
     constructor({config, logger}) {
         this.baseUrl = config.baseUrl;
         this.fallbacks = config.fallbacks;
-        this.logger = logger
+        this.logger = logger;
     }
+
+    static HttpGetRequestFn = (path, params) => (baseUrl) => axios.get(`${baseUrl}${path}`, {params});
+
+    static HttpPostRequestFn = (path, payload) => (baseUrl) => axios.post(`${baseUrl}${path}`, payload);
 
     tryWithFallback = async (requestFn) => {
+        if (!(this.fallbacks && this.fallbacks.length > 0)) {
+            this.logger.warn('No fallbacks found');
+            return null;
+        }
         for (const fallback of this.fallbacks) {
             try {
-                return await requestFn(fallback)
+                return await requestFn(fallback);
             } catch (e) {
-                this.logger.warn(`Failed to get data from fallback ${fallback}, trying next fallback`)
+                this.logger.warn(`Failed to get data from fallback ${fallback}, trying next fallback`);
             }
         }
-        return null
-    }
+        return null;
+    };
 
     get = async (path, params) => {
-        const getReqFn = HttpClient.HttpGetRequestFn(path, params)
+        const getReqFn = HttpClient.HttpGetRequestFn(path, params);
         try {
-            return await getReqFn(this.baseUrl)
+            return await getReqFn(this.baseUrl);
         } catch (err) {
-            if (this.fallbacks && this.fallbacks.length > 0) {
-                this.logger.warn(`Failed to get data from ${this.baseUrl}, trying fallbacks in given order`)
-                const response = await this.tryWithFallback(getReqFn)
-                if (response) {
-                    return response
-                }
-            } else {
-                console.warn(`${err} for ${this.baseUrl}, Mo fallbacks found`)
+            this.logger.warn(`Failed to get data from ${this.baseUrl}, trying fallbacks in given order`);
+            const response = await this.tryWithFallback(getReqFn);
+            if (response) {
+                return response;
             }
-            throw err
+            throw err;
         }
-    }
+    };
 
     post = async (path, payload) => {
-        const postReqFn = HttpClient.HttpPostRequestFn(path, payload)
+        const postReqFn = HttpClient.HttpPostRequestFn(path, payload);
         try {
-            return await postReqFn(this.baseUrl)
+            return await postReqFn(this.baseUrl);
         } catch (err) {
-            this.logger.warn(`Failed to get data from ${this.baseUrl}, trying fallbacks in the given order`)
-            const response = await this.tryWithFallback(postReqFn)
+            this.logger.warn(`Failed to get data from ${this.baseUrl}, trying fallbacks in given order`);
+            const response = await this.tryWithFallback(postReqFn);
             if (response) {
-                return response
+                return response;
             }
-            throw err
+            throw err;
         }
-    }
+    };
 }
 
-module.exports = HttpClient
+module.exports = HttpClient;
