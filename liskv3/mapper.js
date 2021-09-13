8670508ca856d891c2f568739bcc0a2994ab79ef
sacOO7 <sachinshinde7676@gmail.com>
2021-09-13 16:12:33 +0530

    sending signatures back as is, since it will have signerAddress property

--- liskv3/mapper.js
+++ liskv3/mapper.js
@@ -6,8 +6,7 @@ const transactionMapper = ({id, asset : {amount, recipient, data}, sender, block
         timestamp,
         senderAddress : sender.address,
         recipientAddress : recipient.address,
-        // todo - need to check if signature is a signerAddress
-        signatures: signatures.map(address => ({signerAddress: address}))
+        signatures: signatures
     }
 }
 
